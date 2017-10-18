6ea38116db6932f9d08516ad079caa28fa461e6d
Serge Bazanski <q3k@boson.me>
2017-10-18 13:57:38 +0100

    Deploy to Rinkeby (Network ID: 4)

--- dapp/truffle-config.js
+++ dapp/truffle-config.js
@@ -4,6 +4,12 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4,
+      gas: 4612388
     }
   }
 };
