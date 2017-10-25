07f96f965d7691fb5f95c32316a351fcfb0141ff
albertbalinski <albert.balinski@gmail.com>
2017-10-25 20:42:51 +0200

    Get rid off unnecessary css in loan list
    Add cursor pointer to loan list
    Fix warning

--- frontend/src/components/Invest/LoanList/LoanList.vue
+++ frontend/src/components/Invest/LoanList/LoanList.vue
@@ -15,7 +15,7 @@
             </md-table-row>
           </md-table-header>
           <md-table-body>
-            <md-table-row v-for="loan in loans">
+            <md-table-row v-for="loan in loans" :key="loan.userName">
               <md-table-cell class="ll-td">{{ loan.userName }}</md-table-cell>
               <md-table-cell class="ll-td ll-score"> <user-score :value="loan.userScore"/> </md-table-cell>
               <md-table-cell class="ll-td">{{ loan.amountNeeded }}</md-table-cell>
@@ -54,13 +54,8 @@ export default {
 .loan-list { width: calc(100% - 200px); min-width: 570px; margin: 0 auto;
   .ll-title { margin: 54px 0px 30px 25px; font-size: 28px; line-height: 1; letter-spacing: -0.3px; color: var(--color-black-cod); }
   .ll-container { margin-bottom: 40px; border-radius: 2px; background-color: #ffffff; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.02);
-    table.table { width: 100%;
-      th { height: 44px; font-size: 11px; font-weight: 300; text-align: left; letter-spacing: 0.4px; color: #858585; border-bottom: 1px solid var(--color-white-smoke); }
-      td { height: 66px; text-align: left; border-bottom: 1px solid var(--color-white-smoke); }
-      tr { margin-left: 10px; }
-    }
     .ll-th { height: 44px; font-size: 11px; font-weight: 300; letter-spacing: 0.4px; color: #858585; border-bottom: 1px solid var(--color-white-smoke); }
-    .ll-td { height: 66px; font-size: 12px; text-align: left; color: #1c1c1c; border-bottom: 1px solid var(--color-white-smoke);
+    .ll-td { height: 66px; font-size: 12px; text-align: left; color: #1c1c1c; border-bottom: 1px solid var(--color-white-smoke); cursor: pointer;
       &.ll-score { width: 300px; }
       &.ll-time { font-weight: 300; line-height: 1.83; color: #858585; }
     }
