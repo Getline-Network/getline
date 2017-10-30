169e88860747c654c81fa384ee528713431624c2
albertbalinski <albert.balinski@gmail.com>
2017-10-30 16:07:06 +0100

    Change npm to yarn

--- frontend/build/check-versions.js
+++ frontend/build/check-versions.js
@@ -3,7 +3,7 @@ const chalk = require('chalk')
 const semver = require('semver')
 const packageConfig = require('../package.json')
 const shell = require('shelljs')
-function exec (cmd) {
+function exec(cmd) {
   return require('child_process').execSync(cmd).toString().trim()
 }
 
@@ -14,14 +14,6 @@ const versionRequirements = [
     versionRequirement: packageConfig.engines.node
   }
 ]
-
-if (shell.which('npm')) {
-  versionRequirements.push({
-    name: 'npm',
-    currentVersion: exec('npm --version'),
-    versionRequirement: packageConfig.engines.npm
-  })
-}
 
 module.exports = function () {
   const warnings = []
