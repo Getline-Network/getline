a702f4c4a3dcb3cc81ba606156827a7df2f7d9cd
Serge Bazanski <q3k@boson.me>
2017-11-10 15:53:05 +0000

    Deploy contracts to blockchain

--- getline.ts/src/client.ts
+++ getline.ts/src/client.ts
@@ -1,70 +1,238 @@
-import * as jspb from "google-protobuf";
-import {grpc, Code, Metadata} from "grpc-web-client";
-import {BigNumber} from 'bignumber';
+import * as jspb from 'google-protobuf';
+import {grpc, Code, Metadata, Transport} from 'grpc-web-client';
+import {BigNumber} from 'bignumber.js';
+import * as Web3 from 'web3';
+import * as encoding from 'text-encoding';
+import * as moment from 'moment';
 
 import {Metabackend} from "./generated/metabackend_pb_service";
 import * as pb from "./generated/metabackend_pb";
+
+import nodeHttpRequest from 'grpc-web-client/dist/transports/nodeHttp';
 
 export class Loan {
 	public parameters: {
 		collateralTokenAddress: string
 		loanTokenAddress: string
 		liegeAddress: string
+		fundraisingDeadline: moment.Moment
+		paybackDeadline: moment.Moment
 	}
 	public shortId: string
 	public description: string
+	public address: string
+	public owner: string
 
-	constructor(proto: pb.LoanCache) {
+	private blockToTime: (BigNumber) => moment.Moment
+
+	constructor(proto: pb.LoanCache, blockToTime: (BigNumber) => moment.Moment) {
+		this.blockToTime = blockToTime
+
 		this.shortId = proto.getShortId();
-		this.description = "";
+		this.description = proto.getDescription();
+		this.address = proto.getDeploymentAddress().getAscii();
+		this.owner = proto.getOwner().getAscii();
+
 		let params = proto.getParameters();
 		this.parameters = {
 			collateralTokenAddress: params.getCollateralToken().getAscii(),
 			loanTokenAddress: params.getLoanToken().getAscii(),
 			liegeAddress: params.getLiege().getAscii(),
+			fundraisingDeadline: this.blockToTime(new BigNumber(params.getFundraisingBlocksCount())),
+			paybackDeadline: this.blockToTime(new BigNumber(params.getPaybackBlocksCount()))
 		};
 	}
 }
 
-export class Client {
+class MetabackendClient {
     private metabackendHost: string;
     private network: string;
+    private contractDefinitions: Array<pb.Contract> | undefined;
 
-    private async invokeMetabackend<TReq extends jspb.Message, TRes extends jspb.Message>
+    public constructor(host: string, network: string) {
+        this.metabackendHost = host;
+        this.network = network;
+    }
+
+    private async getContractDefinitions(): Promise<Array<pb.Contract>> {
+        if (this.contractDefinitions != undefined) {
+            return this.contractDefinitions;
+        }
+        console.log("getline.ts: downloading contract definitions from metabackend...");
+        let req = new pb.GetDeploymentRequest();
+        req.setNetworkId(this.network);
+        let res = await this.invoke(Metabackend.GetDeployment, req);
+        if (res.getNetworkId() != this.network) {
+            throw new Error("getline.ts: metabackend responded with invalid network");
+        }
+        this.contractDefinitions = res.getContractList();
+        return this.contractDefinitions;
+    }
+
+    public async getABI(name: string): Promise<Array<Web3.AbiDefinition>> {
+        let pbContract: pb.Contract | undefined = undefined;
+        let allContracts = await this.getContractDefinitions();
+        for (let i = 0; i < allContracts.length; i++) {
+            let c = allContracts[i];
+            if (c.getName() == name) {
+                pbContract = c;
+                break
+            }
+        }
+        if (pbContract == undefined) {
+            throw new Error("getline.ts: no contract " + name + " found on metabackend");
+        }
+        if (!pbContract.hasAbi()) {
+            throw new Error("getline.ts: contract " + name + " has no ABI on metabackend");
+        }
+        let json = pbContract.getAbi().getJson_asU8();
+        let jsonString = new encoding.TextDecoder("utf-8").decode(json);
+        return JSON.parse(jsonString);
+    }
+
+    public  async invoke<TReq extends jspb.Message, TRes extends jspb.Message>
                                    (method: grpc.MethodDefinition<TReq, TRes>, req: TReq): Promise<TRes> {
         return new Promise<TRes>((resolve, reject)=>{
             grpc.invoke(method, {
                 request: req,
                 host: this.metabackendHost,
                 onMessage: resolve,
+                transport: nodeHttpRequest,
                 onEnd: (code: Code, msg: string | undefined, trailers: Metadata) => {
                     if (code != Code.OK) {
-                        reject("gRPC failed (" + code + "): " + msg);
+                        reject(new Error("gRPC failed (" + code + "): " + msg));
                     }
                 }
             });
         });
     }
 
-    constructor(metabackend: string, network: string) {
-        this.metabackendHost = metabackend;
-        this.network = network;
+    public async getBytecode(name: string): Promise<string> {
+        let pbContract: pb.Contract | undefined = undefined;
+        let allContracts = await this.getContractDefinitions();
+        for (let i = 0; i < allContracts.length; i++) {
+            let c = allContracts[i];
+            if (c.getName() == name) {
+                pbContract = c;
+                break
+            }
+        }
+        if (pbContract == undefined) {
+            throw new Error("getline.ts: no contract " + name + " found on metabackend");
+        }
+        if (!pbContract.hasAbi()) {
+            throw new Error("getline.ts: contract " + name + " has no ABI on metabackend");
+        }
+        return pbContract.getLinkedBinary();
+    }
+}
+
+
+export class Client {
+    private metabackend: MetabackendClient;
+    private network: string;
+    private web3: Web3;
+    private contractDefinitions: Array<pb.Contract> | undefined;
+
+    public TEST_TOKEN = "0x02c9ccaa1034a64e3a83df9ddce30e6d4bc40515";
+
+    private async deployContract<T extends Web3.ContractInstance>
+                          (contractName: string, ...params: Array<any>): Promise<T> {
+        let abi = await this.metabackend.getABI(contractName);
+        let contract = this.web3.eth.contract(abi);
+        let bytecode = await this.metabackend.getBytecode(contractName);
+
+        return new Promise<T>((resolve, reject)=>{
+            // TODO(q3k): Typify Web3.Contract.new
+            let contractAny: any = contract;
+
+            let opts = {
+                from: this.web3.eth.coinbase,
+                gas: 4612388,
+                data: bytecode,
+            };
+            console.log("getline.ts: deploying contract " + contractName);
+            console.log("getline.ts: deploying bytecode " + bytecode);
+            console.log("getline.ts:    with parameters " + params);
+            let instance: T = contractAny.new(...params, opts, (err, c: T) =>{
+                if (err) {
+                    reject(new Error("deployment failed: " + err));
+                    return;
+                }
+                if (!c.address) {
+                    console.log("getline.ts: deploying...");
+                    return;
+                }
+                console.log("getline.ts: deployed at " + c.address);
+                resolve(contractAny.at(c.address));
+            });
+        });
     }
 
-    public async addNewLoan(description: string, amount: BigNumber, rate: number, paybackTime: number): Promise<Loan> {
-        let address = "0xd6d9520D315CDbC773B6087b0190b97B5BaAd6b3";
 
-        let indexReq = new pb.IndexLoanRequest();
-        indexReq.setDescription(description);
-        indexReq.setNetworkId(this.network);
-        let loanAddress = new pb.Address();
-        loanAddress.setAscii(address);
-        indexReq.setLoan(loanAddress);
+    constructor(metabackend: string, network: string) {
+        this.metabackend = new MetabackendClient(metabackend, network);
+        this.network = network;
+        let provider = new Web3.providers.HttpProvider("http://localhost:8545")
+		if (typeof window !== 'undefined' && typeof window['web3'] !== 'undefined') {
+			console.log("getline.ts: using injected web3")
+			provider = window['web3'].currentProvider;
+		} else {
+			console.log("getline.ts: connecting to node running on localhost")
+		}
+        this.web3 = new Web3(provider);
+		if (this.web3.version.network != this.network) {
+			throw new Error("web3 is connected to wrong network")
+		}
+    }
 
-        let indexRes = await this.invokeMetabackend(Metabackend.IndexLoan, indexReq);
-        console.log("Created Loan ", indexRes.getShortId());
+	private blockToTime = (block: BigNumber): moment.Moment => {
+		if (this.network != "4") {
+            throw new Error("getline.ts only supports rinkeby chains");
+		}
+        let currentBlock = new BigNumber(this.web3.eth.blockNumber);
+		let secondsPerBlock = new BigNumber(15);
 
-        return this.getLoanByShortId(indexRes.getShortId());
+		let seconds = block.minus(currentBlock).times(secondsPerBlock);
+
+		return moment(moment.now()).add(seconds.toNumber(), 'seconds');
+	}
+
+    public async addNewLoan(description: string, amount: BigNumber, interestPermil: number,
+                            fundraisingDelta: number, paybackDelta: number): Promise<Loan> {
+        // TODO(q3k) change this when we're not on rinkeby and we have a better loan SC
+        if (this.network != "4") {
+            throw new Error("cannot place loan on non-rinkeby chains");
+        }
+        if (fundraisingDelta < 0) {
+            throw new Error("cannot place loan with fundraising deadline in the past");
+        }
+        if (paybackDelta < 0) {
+            throw new Error("cannot place loan with payback deadline in the past");
+        }
+        if (paybackDelta < fundraisingDelta) {
+            throw new Error("cannot place loan with payback deadline before fundraising deadline");
+        }
+
+        let currentBlock = this.web3.eth.blockNumber;
+        let blocksPerSecond = (1.0) / 15;
+        let fundraisingEnd = currentBlock + blocksPerSecond * fundraisingDelta;
+        let paybackEnd = currentBlock + blocksPerSecond * paybackDelta;
+        let loan = await this.deployContract('Loan',
+            this.TEST_TOKEN, this.TEST_TOKEN,
+            this.web3.eth.accounts[0],
+             amount, interestPermil, fundraisingEnd, paybackEnd);
+
+        let req = new pb.IndexLoanRequest();
+        req.setNetworkId(this.network);
+        req.setDescription(description);
+        let address = new pb.Address();
+        address.setAscii(loan.address);
+        req.setLoan(address);
+
+        let res = await this.metabackend.invoke(Metabackend.IndexLoan, req);
+        console.log("getline.ts: indexed loan as " + res.getShortId());
+        return this.getLoanByShortId(res.getShortId());
     }
 
     public async getLoanByShortId(shortId: string): Promise<Loan> {
@@ -72,12 +240,12 @@ export class Client {
         req.setNetworkId(this.network);
         req.setShortId(shortId);
 
-        let res = await this.invokeMetabackend(Metabackend.GetLoans, req);
+        let res = await this.metabackend.invoke(Metabackend.GetLoans, req);
         if (res.getNetworkId() != this.network) {
 			throw new Error("Invalid network ID in response.");
         }
 
-        return new Loan(res.getLoanCacheList()[0]);
+        return new Loan(res.getLoanCacheList()[0], this.blockToTime);
     }
 
     public async getLoansByOwner(owner: string): Promise<Array<Loan>> {
@@ -87,14 +255,14 @@ export class Client {
         address.setAscii(owner);
         req.setOwner(address);
 
-        let res = await this.invokeMetabackend(Metabackend.GetLoans, req);
+        let res = await this.metabackend.invoke(Metabackend.GetLoans, req);
         if (res.getNetworkId() != this.network) {
 			throw new Error("Invalid network ID in response.");
         }
 
 		let loans : Array<Loan> = [];
 		res.getLoanCacheList().forEach((elem) => {
-			loans.push(new Loan(elem));
+			loans.push(new Loan(elem, this.blockToTime));
 		});
         return loans;
     }
