9d5f79656f6a0b420ab7fd29b64af55b732d14ef
albertbalinski <albert.balinski@gmail.com>
2017-11-13 16:07:33 +0100

    Add initial version of metabackend call

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,10 +6,18 @@ import VueMaterial from 'vue-material';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import API from './api';
+import { Client } from '../../getline.ts';
 
 Vue.use(VueMaterial);
 Vue.use(VueMaterial.MdIcon);
 
+
+setTimeout(async () => {
+  // setTimeout because we are waiting for metamask extenion to inject the web3 object
+  API.init(Client);
+  await API.addNewLoan();  // eslint-disable-line
+});
 /* Register default purple color */
 Vue.material.registerPalette('app-color-pallete', {
   500: '#7249f7',
