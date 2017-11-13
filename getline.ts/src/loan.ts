47cafedeea60fc82c493952b36f1260378c2e856
Serge Bazanski <q3k@boson.me>
2017-11-13 14:41:27 +0000

    Refactor Loan/Client connector

--- getline.ts/src/loan.ts
+++ getline.ts/src/loan.ts
@@ -4,8 +4,11 @@ import * as moment from 'moment';
 import * as pb from './generated/metabackend_pb';
 
 
-export interface BlockToTime {
-    (current: BigNumber, target: BigNumber): moment.Moment
+// Connector is a way for Loans to access blockchain data - this is provided
+// by the instantiator of the Loan class.
+export interface Connector {
+    blockToTime(current: BigNumber, target: BigNumber): moment.Moment;
+    getCurrentBlock(): Promise<BigNumber>
 }
 
 export class Loan {
@@ -21,23 +24,31 @@ export class Loan {
     public address: string
     public owner: string
 
-    private blockToTime: BlockToTime
- 
-    constructor(proto: pb.LoanCache, currentBlock: BigNumber, blockToTime: BlockToTime) {
-        this.blockToTime = blockToTime
+    private connector: Connector
 
+ 
+    constructor(connector: Connector) {
+        this.connector = connector;
+    }
+
+    public async loadFromProto(proto: pb.LoanCache) {
         this.shortId = proto.getShortId();
         this.description = proto.getDescription();
         this.address = proto.getDeploymentAddress().getAscii();
         this.owner = proto.getOwner().getAscii();
+
+        let currentBlock = await this.connector.getCurrentBlock();
+        let blockToTime = (count: string)=>{
+            return this.connector.blockToTime(currentBlock, new BigNumber(count));
+        }
 
         let params = proto.getParameters();
         this.parameters = {
             collateralTokenAddress: params.getCollateralToken().getAscii(),
             loanTokenAddress: params.getLoanToken().getAscii(),
             liegeAddress: params.getLiege().getAscii(),
-            fundraisingDeadline: this.blockToTime(currentBlock, new BigNumber(params.getFundraisingBlocksCount())),
-            paybackDeadline: this.blockToTime(currentBlock, new BigNumber(params.getPaybackBlocksCount()))
+            fundraisingDeadline: blockToTime(params.getFundraisingBlocksCount()),
+            paybackDeadline: blockToTime(params.getPaybackBlocksCount())
         };
     }
 }
