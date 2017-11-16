17fc380f42df010462c4d4bb87a8d1f7250113f5
albertbalinski <albert.balinski@gmail.com>
2017-11-16 22:50:54 +0100

    Create tiles summary component

---
+++ frontend/src/components/common/TilesSummary.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="tiles-summary">
+    <div class="ts-label">{{ label }}</div>
+    <div class="ts-amount-currency">
+      <div class="ts-amount">{{ amount }}</div>
+      <div class="ts-currency">{{ currency }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TileSummary',
+    props: ['label', 'amount', 'currency'],
+};
+</script>
+
+<style scoped lang="scss">
+ .tiles-summary { margin: 30px;
+  .ts-label { font-size: 11px; font-weight: 300; letter-spacing: 0.4px; color: #858585; }
+  .ts-amount-currency { display: flex; justify-content: center;
+    .ts-amount { font-size: 34px; font-weight: 300; line-height: 1.06; letter-spacing: -0.4px; color: var(--color-black-cod); }
+    .ts-currency { font-size: 18px; font-weight: 300; line-height: 1.22; letter-spacing: -0.2px; color: var(--color-black-cod); }
+  }
+}
+</style>
--- frontend/src/components/invest/MyInvestments/MyInvestments.vue
+++ frontend/src/components/invest/MyInvestments/MyInvestments.vue
@@ -2,20 +2,14 @@
   <div class="my-investments">
     <no-investments v-if="displayNoElements" />
     <div v-if="!displayNoElements" class="mi-summary">
-      <div class="mi-invested">
-        <div class="mi-label"> CURRENTLY INVESTED </div>
-        <div class="mi-amount-currency">
-          <div class="mi-amount"> 145.00 </div>
-          <div class="mi-currency"> ETH </div>
-        </div>
-      </div>
-      <div class="mi-earnings">
-        <div class="mi-label"> EARNINGS </div>
-        <div class="mi-amount-currency">
-          <div class="mi-amount"> 20.00 </div>
-          <div class="mi-currency"> ETH </div>
-        </div>
-      </div>
+      <tiles-summary
+        label="CURRENTLY INVESTED"
+        amount="145.00"
+        currency="ETH" />
+      <tiles-summary
+        label="EARNINGS"
+        amount="20.00"
+        currency="ETH" />
     </div>
     <div class="mi-not-completed-container">
       <my-investment-tile v-for="investment in notCompletedInvestments" :key="investment.id" :investment="investment" />
@@ -31,12 +25,14 @@
 import investments from '@/server/getNotCompletedInvestments'; // TODO remove mocks
 import NoInvestments from './NoInvestments';
 import MyInvestmentTile from './MyInvestmentTile';
+import Summary from '../../common/TilesSummary';
 
 export default {
   name: 'MyInvestments',
   components: {
     'no-investments': NoInvestments,
     'my-investment-tile': MyInvestmentTile,
+    'tiles-summary': Summary,
   },
   data() {
     return {
@@ -62,17 +58,8 @@ export default {
 
 <style scoped lang="scss">
 .my-investments { width: calc(100% - 200px); min-width: 570px; margin: 15px auto 40px;
-  .mi-summary { display: flex; justify-content: center;
-    .mi-invested, .mi-earnings { margin: 30px;
-      .mi-label { font-size: 11px; font-weight: 300; letter-spacing: 0.4px; color: #858585; }
-      .mi-amount-currency { display: flex; justify-content: space-between;
-        .mi-amount { font-size: 34px; font-weight: 300; line-height: 1.06; letter-spacing: -0.4px; color: var(--color-black-cod); }
-        .mi-currency { font-size: 18px; font-weight: 300; line-height: 1.22; letter-spacing: -0.2px; color: var(--color-black-cod); }
-      }
-    }
-  }
-  .mi-not-completed-container { display: flex; justify-content: center; flex-wrap: wrap;
-  }
+  .mi-summary { display: flex; justify-content: center; }
+  .mi-not-completed-container { display: flex; justify-content: center; flex-wrap: wrap; }
   .mi-completed-title { margin: 30px 100px; font-size: 28px; line-height: 1; letter-spacing: -0.3px; color: #101010; color: var(--color-black-cod); }
   .mi-completed-container { display: flex; justify-content: center; flex-wrap: wrap; }
 }
