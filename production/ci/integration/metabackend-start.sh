7e2b6ed218ac11eb758f2d3eac041c43c14dd978
Serge Bazanski <q3k@boson.me>
2017-11-23 12:13:23 +0000

    Remove unused file

--- production/ci/integration/metabackend-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-find /app
-
-#      - "-logtostderr"
-#      - "-db_data_source"
-#      - "user=postgres password=postgresci dbname=postgres host=psql sslmode=disable"
-#      - "-eth_remote"
-#      - "http://ganache:8545"
