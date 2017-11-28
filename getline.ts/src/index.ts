9ec4e34c0076d24bd4f2be19f7d05524adb96d97
Serge Bazanski <q3k@boson.me>
2017-11-28 12:40:32 +0000

    Lint

--- getline.ts/src/index.ts
+++ getline.ts/src/index.ts
@@ -1,3 +1,4 @@
 export {Client} from "./client";
 export {Loan, LoanState} from "./loan";
-export {ProviderLocked, ProviderOnUnsupportedNetwork, ProviderInitializationError, UserVisibleError} from "./blockchain";
+export {ProviderLocked, ProviderOnUnsupportedNetwork,
+    ProviderInitializationError, UserVisibleError} from "./blockchain";
