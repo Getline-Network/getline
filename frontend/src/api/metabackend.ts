3ccdb8a5f9ae2d127e915ac1e5f45a99d45182ae
Serge Bazanski <q3k@boson.me>
2017-11-28 12:03:44 +0000

    Switch metabackend to https endpoints.

--- frontend/src/api/metabackend.ts
+++ frontend/src/api/metabackend.ts
@@ -1,6 +1,6 @@
 const METABACKEND: { URL: string; NETWORK: string } =
   {
-    URL: 'http://0.api.getline.in',
+    URL: 'https://0.api.getline.in',
     NETWORK: '4',
   }
 export default METABACKEND;
