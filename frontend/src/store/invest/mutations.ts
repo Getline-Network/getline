723506100e220ebb800e46ce163c74fa21d2179d
albertbalinski <albert.balinski@gmail.com>
2017-12-13 11:27:12 +0100

    Implement sorting in loans-to-invest view
    After clicking on a table header, we receive an object of type sortColumnT
    Then we pass this object to our state, where we sort our loans
    
    Added tests for it
    Changed amountWanted and amountGathered type from string to number

--- frontend/src/store/invest/mutations.ts
+++ frontend/src/store/invest/mutations.ts
@@ -1,33 +1,83 @@
 import Vue from 'vue'
 import { BigNumber } from 'bignumber.js/bignumber';
+import * as moment from "moment";
 
-import { InvestStateT, LoanToInvestT } from './types';
+import { InvestStateT, LoanToInvestT, sortColumnT } from './types';
 
 export const mutations = {
   'RECEIVE_LOANS_TO_INVEST': function (state: InvestStateT, { loans }: { loans: LoanToInvestT[] }): void {
     state.loansToInvest = loans.map(extendLoan)
     state.isLoading = false;
   },
-  'REQUEST_LOANS_TO_INVEST': function (state): void {
+  'REQUEST_LOANS_TO_INVEST': function (state: InvestStateT): void {
     state.isLoading = true;
   },
+  'SORT_LOANS_TO_INVEST': function (state: InvestStateT, byColumn: sortColumnT): void {
+    state.loansToInvest.sort(getSortCmp(byColumn));
+  }
+}
+
+function getSortCmp(column: sortColumnT) {
+  const sortType: string = column.type;
+  function reverseIfDesc(sub: number): number {
+    return sortType == 'asc' ? sub : -sub;
+  }
+  switch (column.name) {
+    case "AMOUNT_WANTED":
+      return function (a: LoanToInvestT, b: LoanToInvestT): number {
+        const sub = a.amountWanted.sub(b.amountWanted).toNumber();
+        return reverseIfDesc(sub)
+      }
+
+    case "TIME":
+      return function (a: LoanToInvestT, b: LoanToInvestT): number {
+        const sub = a.fundraisingDeadline.unix() - b.fundraisingDeadline.unix();
+        return reverseIfDesc(sub);
+      }
+
+    case "FUNDED":
+      return function (a: LoanToInvestT, b: LoanToInvestT): number {
+        const percentageA = getPercentageFunded(a);
+        const percentageB = getPercentageFunded(b);
+        const sub = percentageA - percentageB;
+        return reverseIfDesc(sub);
+      }
+
+    case "NEEDED":
+      return function (a: LoanToInvestT, b: LoanToInvestT): number {
+        const percentageA = getPercentageWanted(a);
+        const percentageB = getPercentageWanted(b);
+        const sub = percentageA - percentageB;
+        return reverseIfDesc(sub);
+      }
+
+    case "NAME":
+      return function (a: LoanToInvestT, b: LoanToInvestT): number {
+        let sub = 0;
+        if (a.userName < b.userName) {
+          sub = -1
+        }
+        if (a.userName > b.userName) {
+          sub = 1
+        }
+        return reverseIfDesc(sub);
+      }
+  }
 }
 
 function extendLoan(loan: LoanToInvestT): LoanToInvestT {
   return {
     ...loan,
     amountWantedWithToken: loan.amountWanted.toString() + " " + loan.tokenSymbol,
-    percentageFunded: getPercentageFunded(loan.amountGathered, loan.amountWanted),
-    percentageWanted: getPercentageWanted(loan.amountGathered, loan.amountWanted),
+    percentageFunded: getPercentageFunded(loan),
+    percentageWanted: getPercentageWanted(loan),
   }
 }
 
-function getPercentageFunded(amountGathered: BigNumber, amountWanted: BigNumber): string {
-  const percentage = amountGathered.times(100).dividedBy(amountWanted);
-  return percentage + "%"
+function getPercentageFunded(loan: LoanToInvestT): number {
+  return loan.amountGathered.times(100).dividedBy(loan.amountWanted).toNumber();
 }
 
-function getPercentageWanted(amountGathered: BigNumber, amountWanted: BigNumber): string {
-  const percentage = (new BigNumber(100)).sub(amountGathered.times(100).dividedBy(amountWanted));
-  return percentage + "%"
+function getPercentageWanted(loan: LoanToInvestT): number {
+  return (new BigNumber(100)).sub(loan.amountGathered.times(100).dividedBy(loan.amountWanted)).toNumber();
 }
--- frontend/src/store/invest/types.ts
+++ frontend/src/store/invest/types.ts
@@ -1,4 +1,5 @@
 import { BigNumber } from 'bignumber.js/bignumber';
+import * as moment from "moment";
 export interface InvestStateT {
   loansToInvest: LoanToInvestT[];
   isLoading: boolean;
@@ -7,15 +8,20 @@ export interface InvestStateT {
 export interface LoanToInvestT {
   id: string;
   userName: string;
-  fundraisingDeadline: string;
+  fundraisingDeadline: moment.Moment;
   amountGathered: BigNumber;
   amountWanted: BigNumber;
   amountWantedWithToken?: string,
   tokenSymbol: string;
   interestPermil: number;
-  percentageFunded?: string;
-  percentageWanted?: string;
+  percentageFunded?: number;
+  percentageWanted?: number;
 };
+
+export interface sortColumnT {
+  name: string; // in DOM md-table-head.md-sorty-by
+  type: string; // 'asc' or 'desc'
+}
 
 import { LoanState } from '../../../../getline.ts';
 export { LoanState } from '../../../../getline.ts';
