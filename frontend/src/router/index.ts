56dc2f9f697963bac8c78c4a9ff0b82fcd5b6fc4
albertbalinski <albert.balinski@gmail.com>
2017-12-20 07:43:22 +0100

    Add title to pages

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,5 +5,11 @@ import routing from './routing';
 
 Vue.use(Router);
 
-export default new Router(routing);
+const router = new Router(routing);
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})
+
+export default router;
 export * from './redirects';
--- frontend/src/router/routing.ts
+++ frontend/src/router/routing.ts
@@ -20,19 +20,26 @@ const routing: RouterOptions = {
     {
       path: '/print',
       component: PrintDemoTokens,
+      meta: { title: 'Print some test tokens' }
+
     },
     {
       path: '/borrow',
       component: RequestLoan,
+      meta: { title: 'Request a loan' }
+
     },
     {
       path: '/my-loans',
       component: MyLoans,
+      meta: { title: 'My loans' }
+
     },
     {
       path: '/invest/loan/:shortId',
       component: Investment,
       props: { hideInvestBar: true },
+      meta: { title: 'Invest in a loan' }
     },
     {
       path: '/invest',
@@ -44,28 +51,34 @@ const routing: RouterOptions = {
         {
           path: 'loan-list',
           component: LoanList,
+          meta: { title: 'List of loans' }
         },
         {
           path: 'my-investments',
           component: MyInvestments,
+          meta: { title: 'My investments' }
         },
       ],
     },
     {
       path: '/error/install-metamask/',
       component: InstallMetamask,
+      meta: { title: 'Please install metamask' }
     },
     {
       path: '/error/unlock-metamask/',
       component: UnlockMetamask,
+      meta: { title: 'Please unlock metamask' }
     },
     {
       path: '/error/network/',
       component: BadNetwork,
+      meta: { title: 'Bad network' }
     },
     {
       path: '/error/unknown/',
       component: UnknownError,
+      meta: { title: 'Unknown error' }
     },
   ],
 }
