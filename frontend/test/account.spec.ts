a86f617357f840d55c094b5bdd6fbc8ab5b6a905
Serge Bazanski <q3k@boson.me>
2018-01-10 11:47:02 +0000

    Fix balance spec tests

--- frontend/test/account.spec.ts
+++ frontend/test/account.spec.ts
@@ -6,16 +6,21 @@ import { AccountStateT } from '../src/store/account/types';
 import { } from 'jasmine'; // For describe(...) and it(...) types
 
 describe('Loading account balance', () => {
-  it('Should get my ballance', () => {
-    const state: AccountStateT = { balance: "", isLoading: false, loggedIn: true, balanceTokenName: "T", demoPrintValue: "123" };
+  it('should get my balance', () => {
+  const state: AccountStateT = { isLoading: false, loggedIn: true, balance: { balance: new BigNumber(0), tokenSymbol: "T", tokenName: "T", demoPrintValue: new BigNumber(0) }};
 
     mutations['REQUEST_MY_BALANCE'](state);
     expect(state.isLoading).to.equal(true)
 
-    mutations['RECEIVE_MY_BALANCE'](state, { balance: "12", tokenName: "ABCTOKEN", demoPrintValue: "123" });
+    const newBalance = {
+      balance: new BigNumber(12),
+      tokenName: "ABCTOKEN",
+      tokenSymbol: "ABC", 
+      demoPrintValue: new BigNumber(1000),
+    }
+    mutations['RECEIVE_MY_BALANCE'](state, newBalance);
+
     expect(state.isLoading).to.equal(false)
-    const bal: string = state.balance;
-    expect(bal).to.equal("12");
-    expect(state.balanceTokenName).to.equal("ABCTOKEN");
+    expect(state.balance).to.equal(newBalance);
   })
 })
